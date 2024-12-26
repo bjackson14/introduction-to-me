b58d1062f7e0635da0bef7ae39bcb7033f21c98e
Ben Jackson <ben.jksn@gmail.com>
2024-12-26 16:13:06 -0500

    itm-29 unti converter updated to be more accessible

--- unit-converter/unit-converter.js
+++ unit-converter/unit-converter.js
@@ -8,16 +8,19 @@ const conversions = [
 ]
 
 convertBtn.addEventListener('click', () => {
-  conversionContainer.innerHTML = '';
   let numberToConvert = conversionInput.value;
   let calculationsHtml = '';
-  conversions.forEach((conversion) => {
-    calculationsHtml += `
-      <div class="conversion-container">
-        <p class="unit-type">${conversion.type.charAt(0).toUpperCase()}${conversion.type.slice(1)} (${conversion.metric.charAt(0).toUpperCase()}${conversion.metric.slice(1)}/${conversion.imperial.charAt(0).toUpperCase()}${conversion.imperial.slice(1)})</p>
-        <p class="unit-data">${numberToConvert} ${conversion.shortMetric ? conversion.shortMetric : conversion.metric} = ${(conversion.type === 'volume' ? numberToConvert / conversion.conversion : numberToConvert * conversion.conversion).toFixed(3)} ${conversion.imperial} | ${numberToConvert} ${conversion.imperial} = ${(conversion.type === 'volume' ? numberToConvert * conversion.conversion : numberToConvert / conversion.conversion).toFixed(3)} ${conversion.shortMetric ? conversion.shortMetric : conversion.metric}</p>
-      </div>
-    `;
-  });
-  conversionContainer.innerHTML += calculationsHtml;
+
+  conversionContainer.innerHTML = '';
+  if (numberToConvert) {
+    conversions.forEach((conversion) => {
+      calculationsHtml += `
+        <div class="conversion-value-container">
+          <p class="unit-type">${conversion.type.charAt(0).toUpperCase()}${conversion.type.slice(1)} (${conversion.metric.charAt(0).toUpperCase()}${conversion.metric.slice(1)}/${conversion.imperial.charAt(0).toUpperCase()}${conversion.imperial.slice(1)})</p>
+          <p class="unit-data">${numberToConvert} ${conversion.shortMetric ? conversion.shortMetric : conversion.metric} = ${(conversion.type === 'volume' ? numberToConvert / conversion.conversion : numberToConvert * conversion.conversion).toFixed(3)} ${conversion.imperial} | ${numberToConvert} ${conversion.imperial} = ${(conversion.type === 'volume' ? numberToConvert * conversion.conversion : numberToConvert / conversion.conversion).toFixed(3)} ${conversion.shortMetric ? conversion.shortMetric : conversion.metric}</p>
+        </div>
+      `;
+    });
+    conversionContainer.innerHTML += calculationsHtml;
+  }
 });
