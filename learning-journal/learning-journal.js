4094dfe1ddb719fc878d0a7cb8b72c384c545546
Ben Jackson <ben.jksn@gmail.com>
2025-01-22 20:49:22 -0500

    itm-57 Formated about me for desktop

--- learning-journal/learning-journal.js
+++ learning-journal/learning-journal.js
@@ -1,12 +1,3 @@
-const viewMoreBtn = document.getElementById('view-more-btn');
 const copyright = document.getElementById('copyright');
-
-viewMoreBtn.addEventListener('click', () => {
-  const articles = document.getElementsByTagName('article');
-
-  for (let article of articles) {
-    article.classList.remove('hidden');
-  }
-});
 
 copyright.textContent = `Copyright ©${new Date().getFullYear()}`
