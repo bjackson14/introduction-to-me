4932913f16a4acea6b52b5bf0146eb40d5aa1b87
Ben Jackson <ben.jksn@gmail.com>
2025-02-17 21:51:14 -0500

    itm-74 Added color grid

--- color-picker/color-picker.js
+++ color-picker/color-picker.js
@@ -1,10 +1,35 @@
 const colorBtn = document.getElementById('color-btn');
 const colorInput = document.getElementById('color-input');
 const modeSelect = document.getElementById('mode-select');
+const colorGrid = document.getElementById('color-grid');
 
-colorBtn.addEventListener('click', event => {
-  let color = colorInput.value.slice(1);
+colorBtn.addEventListener('click', () => {
+  const color = colorInput.value.slice(1);
+
+  colorGrid.innerHTML = '';
   fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${modeSelect.value}&count=5`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+      data.colors.forEach(color => {
+        addColorToContainer(color.hex.value);
+      });
+    });
 });
+
+function addColorToContainer(color) {
+  const colorContainer = document.createElement('div');
+  const colorDiv = document.createElement('div');
+  const hexCode = document.createElement('div');
+
+  colorContainer.classList.add('color-container')
+
+  colorDiv.classList.add('color')
+  colorDiv.style.backgroundColor = color;
+
+  hexCode.classList.add('hex-code')
+  hexCode.textContent = color;
+
+  colorContainer.appendChild(colorDiv);
+  colorContainer.appendChild(hexCode);
+  colorGrid.appendChild(colorContainer);
+}
