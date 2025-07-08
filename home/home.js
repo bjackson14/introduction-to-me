5bd37b968fa7803cfbee70597fc5be4386a4516b
Ben Jackson <ben.jksn@gmail.com>
2025-07-07 23:26:46 -0400

    itm-6 created about section

--- home/home.js
+++ home/home.js
@@ -1,19 +1,22 @@
 const headerMenu = document.getElementById('header-menu');
 const headerMenuBtn = document.getElementById('header-menu-btn');
-const hamburgerMenuBtn = document.getElementById('hamburger-menu-btn');
-const closeMenuBtn = document.getElementById('close-menu-btn');
+const hamburgerMenuIcon = document.getElementById('hamburger-menu-icon');
+const closeMenuIcon = document.getElementById('close-menu-icon');
 
 let isHamburgerMenuClosed = false;
 
-headerMenuBtn.addEventListener('click', () => {
+headerMenuBtn.addEventListener('click', toggleMenu)
+headerMenu.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
   if (isHamburgerMenuClosed) {
     headerMenu.style.display = "none";
-    hamburgerMenuBtn.style.display = "block";
-    closeMenuBtn.style.display = "none";
+    hamburgerMenuIcon.style.display = "block";
+    closeMenuIcon.style.display = "none";
   } else {
     headerMenu.style.display = "block";
-    hamburgerMenuBtn.style.display = "none";
-    closeMenuBtn.style.display = "block";
+    hamburgerMenuIcon.style.display = "none";
+    closeMenuIcon.style.display = "block";
   }
   isHamburgerMenuClosed = !isHamburgerMenuClosed;
-})
+}
